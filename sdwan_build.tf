03b413985229ae5a1e92e3c5e733972ff73e47e7
hyeomans-cisco <hyeomans@cisco.com>
2019-08-28 20:48:45 -0400

    commit up intial tf

---
+++ sdwan_build.tf
@@ -0,0 +1,15 @@
+# variables
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+# provider
+provider "aws" {
+    access_key = "access_key"
+    secret_key = "secret_key"
+    region = "us-east-1"
+}
+
+# resource
+
+# output
+
