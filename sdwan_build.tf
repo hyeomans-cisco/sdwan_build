4868daf01c7dcdd52d5833c5b690dd4a8d5e2a99
hyeomans-cisco <hyeomans@cisco.com>
2019-10-25 12:27:06 -0400

    Added inter cidr communication in security group

--- sdwan_build.tf
+++ sdwan_build.tf
@@ -187,7 +187,15 @@ resource "aws_security_group" "sdwan-cisco-ips-sg" {
         to_port = 443
         protocol = "tcp"
         cidr_blocks = ["173.36.0.0/14"]
+    } 
+
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["${var.vpc_cidr_block}"]
     }
+    
     egress {
         from_port = 0
         to_port = 0
